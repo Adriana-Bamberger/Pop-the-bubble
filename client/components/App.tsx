f70a9b0736d5f91672d9169f7edd049a8ad4479b
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-05-10 11:19:51 +1200

    fix header css

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -7,8 +7,6 @@ import Footer from './Footer'
 function App() {
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
       <Header />
       <Score />
       <Body />
