60878e43ef4213c98e146d676e5ffb3560081b9b
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-05-10 10:37:33 +1200

    added components to App

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,19 @@
+import Header from './Header'
+import Score from './Score'
+import Body from './Body'
+import Grid from './Grid'
+import Footer from './Footer'
+
 function App() {
   return (
     <div>
       <h1>App</h1>
       <p>React development has begun!</p>
+      <Header />
+      <Score />
+      <Body />
+      <Grid />
+      <Footer />
     </div>
   )
 }
