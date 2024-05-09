fec3b2bb2c0c690ea77555218ee3e77cb487d216
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-05-10 11:44:30 +1200

    score in the header

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,5 +1,4 @@
 import Header from './Header'
-import Score from './Score'
 import Body from './Body'
 import Footer from './Footer'
 
@@ -7,7 +6,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Score />
+
       <Body />
       <Footer />
     </div>
