bd6e3bf7cfed80de13376908ac68a343bcdb61a8
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-05-10 11:35:44 +1200

    formatting

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> ebf87f30f016ef8627c40a0e6682d98b15e7c805
 import Header from './Header'
 import Score from './Score'
 import Body from './Body'
@@ -11,11 +7,6 @@ import Footer from './Footer'
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-=======
-      <h1>App</h1>
-      <p>React development has begun!</p>
->>>>>>> ebf87f30f016ef8627c40a0e6682d98b15e7c805
       <Header />
       <Score />
       <Body />
