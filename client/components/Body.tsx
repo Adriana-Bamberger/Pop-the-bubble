1f853e9c7f4ae71b3a26bea8f84a817801bac97c
brad-craig <134657831+bradacraig@users.noreply.github.com>
2024-05-10 10:02:19 +1200

    Added inital components

---
+++ client/components/Body.tsx
@@ -0,0 +1,11 @@
+function Body() {
+  return (
+    <div>
+      <h1>Body</h1>
+      <p>grid will live in here</p>
+      <p>THIS WILL BE A BUTTON</p>
+    </div>
+  )
+}
+
+export default Body
---
+++ client/components/Score.tsx
@@ -0,0 +1,10 @@
+function Score() {
+  return (
+    <div>
+      <h1>Score</h1>
+      <p>Make something that counts!</p>
+    </div>
+  )
+}
+
+export default Score
