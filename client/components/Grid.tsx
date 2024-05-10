aa7dcf21d4298d4cf9093a10050f4b4aecfa9843
Brad Craig <brad.a.craig@gmail.com>
2024-05-10 14:12:18 +1200

    created new dynamic array grid

--- client/components/Grid.tsx
+++ client/components/Grid.tsx
@@ -1,43 +1,15 @@
+import React, { useState, useEffect } from 'react'
+
 function Grid() {
+  const [Cell, setCell] = useState<boolean[]>(new Array(25).fill(false))
+
   return (
-    <div>
-      <table>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-        </tr>
-      </table>
+    <div className="grid-container">
+      {Cell.map((isCell, index) => (
+        <div className="grid-cell" key={index}>
+          {isCell}
+        </div>
+      ))}
     </div>
   )
 }
