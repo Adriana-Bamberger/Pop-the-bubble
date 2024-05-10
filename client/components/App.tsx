a5bed8506e4a1a3d0584929fb27d591bd10caa83
Felix-XP <134016801+Felix-XP@users.noreply.github.com>
2024-05-10 12:56:10 +1200

    ballons

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,17 +1,30 @@
-import Header from './Header'
-import Body from './Body'
-import Footer from './Footer'
-import StartButton from './StartButton.jsx'
+import Header from './Header.js'
+import Body from './Body.js'
+import Footer from './Footer.js'
 import Score from './Score.js'
+import { useState } from 'react'
+
+function StartGame() {
+  const [start, setStart] = useState(false)
+  return (
+    <>
+      <nav>
+        <button onClick={() => setStart(true)}>Start Game</button>
+      </nav>
+
+      <main>{start === true && <App />}</main>
+    </>
+  )
+}
 
 function App() {
   return (
     <div>
+      <div>test</div>
       <Header />
-      <div>
-        <Score />
-        <StartButton />
-      </div>
+
+      <Score />
+
       <Body />
       <Footer />
     </div>
--- client/components/StartButton.jsx
+++
@@ -1,9 +0,0 @@
-function StartButton() {
-  return (
-    <div>
-      <button>Press</button>
-    </div>
-  )
-}
-
-export default StartButton
