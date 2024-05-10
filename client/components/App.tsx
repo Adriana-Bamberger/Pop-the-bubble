777363bbb70df58350486095d06182bd7fa423b3
Valerian <163621755+valerian-norman@users.noreply.github.com>
2024-05-10 13:18:59 +1200

    StartGame component added and linked to <Body/>

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,32 +2,22 @@ import Header from './Header.js'
 import Body from './Body.js'
 import Footer from './Footer.js'
 import Score from './Score.js'
+import StartGame from './StartGame.js'
 import { useState } from 'react'
 
-function StartGame() {
+function App() {
   const [start, setStart] = useState(false)
+
   return (
     <>
-      <nav>
-        <button onClick={() => setStart(true)}>Start Game</button>
-      </nav>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+      <button onClick={() => setStart(true)}>Start Game</button>
 
-      <main>{start === true && <App />}</main>
+      <main>{start === true && <StartGame />}</main>
     </>
-  )
-}
-
-function App() {
-  return (
-    <div>
-      <div>test</div>
-      <Header />
-
-      <Score />
-
-      <Body />
-      <Footer />
-    </div>
   )
 }
 
---
+++ client/components/StartGame.tsx
@@ -0,0 +1,11 @@
+import Header from './Header.js'
+import Body from './Body.js'
+import Footer from './Footer.js'
+import Score from './Score.js'
+export default function StartGame() {
+  return (
+    <>
+      <Body />
+    </>
+  )
+}
